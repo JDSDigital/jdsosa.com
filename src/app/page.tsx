4bed82264f5eb8fa2dc20e9899242047d4a7a984
Jorge Daniel Sosa <jdsosa@gmail.com>
2023-11-06 22:00:33 +0000

    Feat: Adds initial colors and links

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import styles from "./page.module.css";
 
 import { Big_Shoulders_Display, Michroma } from "next/font/google";
@@ -11,15 +12,20 @@ const microma = Michroma({ subsets: ["latin"], weight: "400" });
 export default function Home() {
   return (
     <main className={styles.main}>
-      <h1
-        className={big_shoulders_display.className}
-        style={{ fontSize: "500px" }}
-      >
+      <h1 className={`${big_shoulders_display.className} ${styles.title}`}>
         JDS
       </h1>
-      <h1 className={microma.className} style={{ fontSize: "50px" }}>
+      <h1 className={`${microma.className} ${styles.subtitle}`}>
         JORGE DANIEL SOSA
       </h1>
+      <div className={styles.logos}>
+        <a href="https://github.com/JDSDigital" target="_blank">
+          <Image alt="github" src="/github.svg" width="50" height="50" />
+        </a>
+        <a href="https://www.linkedin.com/in/jdsosa" target="_blank">
+          <Image alt="linkedin" src="/linkedin.svg" width="50" height="50" />
+        </a>
+      </div>
     </main>
   );
 }
